3b01415fb16cf96ec36ac34b4d1dcf95594a4465
Dave Bishop <david.b.bishop@gmail.com>
2020-08-17 16:26:39 -0500

    Repository fix

--- E3 Code/E3 Code/FakeEmployeeRepository.cs
+++ E3 Code/E3 Code/FakeEmployeeRepository.cs
@@ -3,14 +3,28 @@ namespace E3_Code
 {
     public class FakeEmployeeRepository : IEmployeeRepository
     {
-        Dictionary<int, Employee> Employees = new Dictionary<int, Employee>();
+        private static Dictionary<int, Employee> Employees;
         public FakeEmployeeRepository()
         {
-            // Add some default employees to work with
-            Employees.Add(1, new Employee { Id = 1, LastName = "Bishop", FirstName = "Barb",
-                                            Salary = 100000.00M});
-            Employees.Add(2, new Employee { Id = 2, LastName = "Bishop", FirstName = "Dave",
-                                            Salary = 99999.00M});
+            if (Employees == null)
+            {
+                 Employees = new Dictionary<int, Employee>();
+                // Add some default employees to work with
+                Employees.Add(1, new Employee
+                {
+                    Id = 1,
+                    LastName = "Bishop",
+                    FirstName = "Barb",
+                    Salary = 100000.00M
+                });
+                Employees.Add(2, new Employee
+                {
+                    Id = 2,
+                    LastName = "Bishop",
+                    FirstName = "Dave",
+                    Salary = 99999.00M
+                });
+            }
         }
        private int GetNextId()
         {
